b163a84a4d19f09677362d316a03aab72b1d502a
MaksFULL <169079873+MaksFULL@users.noreply.github.com>
2024-12-18 15:11:05 +0200

    fix problem

--- src/App.jsx
+++ src/App.jsx
@@ -4,46 +4,64 @@ import Options from "./components/Options/Options";
 import Feedback from "./components/Feedback/Feedback";
 import Notification from "./components/Notification/Notification";
 
-
 const App = () => {
 
   const [clicks, setClicks] = useState(() => {
     const savedGrades = localStorage.getItem("saved-grades");
-    if (savedGrades === null) return {
+    return savedGrades ? JSON.parse(savedGrades) : {
       good: 0,
       neutral: 0,
       bad: 0,
     };
-    else { return JSON.parse(savedGrades) };
   });
 
   const totalFeedback = clicks.good + clicks.neutral + clicks.bad;
 
+  const positiveFeedbackPercentage = totalFeedback 
+    ? Math.round((clicks.good / totalFeedback) * 100) 
+    : 0;
+
   const updateFeedback = feedbackType => {
-    setClicks((oldClicks) => ({
+    setClicks(oldClicks => ({
       ...oldClicks,
       [feedbackType]: oldClicks[feedbackType] + 1,
     }));
   }
+
   const resetFeedback = () => {
     setClicks({
       good: 0,
       neutral: 0,
-      bad: 0
-    })
+      bad: 0,
+    });
   };
 
   useEffect(() => {
     localStorage.setItem("saved-grades", JSON.stringify(clicks));
   }, [clicks]);
 
-  return <div className="container">
-    <Description name="Sip Happens Café"
-      text="Please leave your feedback about our service by selecting one of the options below." />
-    <Options update={updateFeedback} reset={resetFeedback} total={totalFeedback} />
-    {totalFeedback ? <Feedback clicks={clicks} total={totalFeedback} /> : <Notification />}
-
-  </div>
+  return (
+    <div className="container">
+      <Description 
+        name="Sip Happens Café"
+        text="Please leave your feedback about our service by selecting one of the options below." 
+      />
+      <Options 
+        update={updateFeedback} 
+        reset={resetFeedback} 
+        total={totalFeedback} 
+      />
+      {totalFeedback ? (
+        <Feedback 
+          clicks={clicks} 
+          total={totalFeedback} 
+          positivePercentage={positiveFeedbackPercentage} 
+        />
+      ) : (
+        <Notification />
+      )}
+    </div>
+  );
 };
 
 export default App;
--- src/components/Feedback/Feedback.jsx
+++ src/components/Feedback/Feedback.jsx
@@ -1,21 +1,29 @@
 import s from "./Feedback.module.css";
-import {FcBullish} from "react-icons/fc";
+import { FcBullish } from "react-icons/fc";
 
-const Feedback = ({ clicks, total}) => {
-    return <div className={s.box}><ul className={s.list}>
+const Feedback = ({ clicks, total, positivePercentage }) => {
+  return (
+    <div className={s.box}>
+      <ul className={s.list}>
         <li className={s.item}>
-            Good: <span classNamr={s.count}>{clicks.neutral}</span>
+          Good: <span className={s.count}>{clicks.good}</span>
         </li>
         <li className={s.item}>
-            Bad: <span className={s.count}>{clicks.bad}</span>
+          Neutral: <span className={s.count}>{clicks.neutral}</span>
         </li>
         <li className={s.item}>
-            Total: <span className={s.count}>{total}</span>
+          Bad: <span className={s.count}>{clicks.bad}</span>
         </li>
         <li className={s.item}>
-            Positive: <span className={s.count}>{Math.round(((clicks.good + clicks.neutral) / total) * 100)}%</span>
+          Total: <span className={s.count}>{total}</span>
         </li>
-    </ul><FcBullish className={s.icon} /></div>
+        <li className={s.item}>
+          Positive: <span className={s.count}>{positivePercentage}%</span>
+        </li>
+      </ul>
+      <FcBullish className={s.icon} />
+    </div>
+  );
 }
 
 export default Feedback;
